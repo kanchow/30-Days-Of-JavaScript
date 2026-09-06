7b6d8ba94cd47ac912015677db4005078e2737e7
chow <zgy1299408744@163.com>
2026-09-06 22:42:04 +0800

    typo

--- 04_Day_Conditionals/04_day_starter/scripts/main.js
+++ 04_Day_Conditionals/04_day_starter/scripts/main.js
@@ -1,3 +1,3 @@
-// this is your main.js script
+// This is your main.js script.
 
-alert('Open the browser console whenever you work on JavaScript')
+alert('Open the browser console whenever you work on JavaScript.')
