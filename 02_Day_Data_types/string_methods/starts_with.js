8bd2e07c45f83bd4facdb33b03bfaac8143ba2bf
chow <zgy1299408744@163.com>
2026-09-06 22:45:39 +0800

    typo

--- 02_Day_Data_types/string_methods/starts_with.js
+++ 02_Day_Data_types/string_methods/starts_with.js
@@ -7,7 +7,7 @@
   string.startsWith(substring)
 */
 
-let string = 'Love is the best to in this world'
+let string = 'Love is the best thing in this world'
 console.log(string.startsWith('Love'))  // true
 console.log(string.startsWith('love'))  // false
 console.log(string.startsWith('world')) // false
@@ -15,4 +15,4 @@ console.log(string.startsWith('world')) // false
 let country = 'Finland'
 console.log(country.startsWith('Fin'))  // true
 console.log(country.startsWith('fin'))  // false
-console.log(country.startsWith('land')) //  false
+console.log(country.startsWith('land')) // false
