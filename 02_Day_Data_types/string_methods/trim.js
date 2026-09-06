a299fc80f506c7d6be4e56f76da3c118cee91176
chow <zgy1299408744@163.com>
2026-09-06 22:47:20 +0800

    typo

--- 02_Day_Data_types/string_methods/trim.js
+++ 02_Day_Data_types/string_methods/trim.js
@@ -1,4 +1,4 @@
-// trim(): Removes trailing space in the beginning or the end of a string.
+// trim(): Removes leading and trailing whitespace from a string.
 
 let string = '   30 Days Of JavaScript   '
 console.log(string)
